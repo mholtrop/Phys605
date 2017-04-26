77f9b17c6874e647e8dd6bf9e6ae20427abc1393
Maurik Holtrop <maurik@physics.unh.edu>
2017-04-26 17:06:17 -0400

    Add module to read MCP320x with hardware SPI

--- Python/Phys605/__init__.py
+++ Python/Phys605/__init__.py
@@ -2,7 +2,9 @@
 # Here we initialize the modules.
 #
 
-__all__ = ["MAX7219"]
+__all__ = ["MAX7219","MCP320x","MCP320xspi"]
 
-from MAX7219 import MAX7219
-from MCP320x import MCP320x
+from MAX7219    import MAX7219
+from MCP320x    import MCP320x
+from MCP320xspi import MCP320xspi
+
