a53bada3b298b301c3951019dd4c25dd91ec8f4c
Maurik Holtrop <maurik@physics.unh.edu>
2017-04-27 13:58:49 -0400

    Add simple implementation for ISL29125 RGB light sensor.

--- Python/Phys605/__init__.py
+++ Python/Phys605/__init__.py
@@ -2,9 +2,9 @@
 # Here we initialize the modules.
 #
 
-__all__ = ["MAX7219","MCP320x","MCP320xspi"]
+__all__ = ["MAX7219","MCP320x","MCP320xspi","ISL29125"]
 
 from MAX7219    import MAX7219
 from MCP320x    import MCP320x
 from MCP320xspi import MCP320xspi
-
+from ISL29125   import ISL29125
