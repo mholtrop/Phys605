052ddb7cfa0a956d975b16fff2ae1f5b513180e3
Maurik Holtrop <maurik@physics.unh.edu>
2018-03-09 15:59:38 -0500

    Add HW demo module

--- Python/DevLib/MAX7219.py
+++ Python/DevLib/MAX7219.py
@@ -8,31 +8,47 @@
 # Author: Maurik Holtrop
 #
 import RPi.GPIO as GPIO
+from spidev import SpiDev
 import time
 
 class MAX7219:
     def __init__(self,DATA_pin,CS_bar_pin,CLK_pin,mode=1):
-        '''This class helps with driving a MAX7219 LED module using regular GPIO pins.
-        You need to initialze the class with the pin numbers for the Clock (clk),
-        Data(dat), and Chip Select bar (cs_bar) pins. This code expects
-        the numbers in the BSM standard.
-        The Raspberry Pi interfacing is done through the RPi.GPIO module.'''
+        '''This class helps with driving a MAX7219 LED module using either regular
+        GPIO pins, or SPI hardware interface.
+        For SPI hardware, set DATA_pin=0, CS_bar_pin=0 or 1, CLK_pin= bus speed (1000000)
+        For GPIO "bit-bang" interface, set DATA_pin = Data in (dat),
+        CS_bar = to Chip Select (cs), and CLK_pin= Clock (clk)
+        A final argument, mode=1 (default) sets number decoding, while
+        mode=0 sets raw mode, see the MAX7219 chip data sheet.
+        -------
+        This code expects the pin numbers in the BSM standard.
+        The Raspberry Pi interfacing is done through the RPi.GPIO module
+        or the spi module'''
+
         self.DATA = DATA_pin
         self.CS_bar = CS_bar_pin
         self.CLK = CLK_pin
         self.Mode = mode
+        self._dev = None
 
-        if GPIO.getmode() != 11:
-            GPIO.setmode(GPIO.BCM)
+        if self.DATA>0:
+            if GPIO.getmode() != 11:
+                GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(self.CLK,GPIO.OUT)
-        GPIO.setup(self.DATA,GPIO.OUT)
-        GPIO.setup(self.CS_bar,GPIO.OUT)
+                GPIO.setup(self.CLK,GPIO.OUT)
+                GPIO.setup(self.DATA,GPIO.OUT)
+                GPIO.setup(self.CS_bar,GPIO.OUT)
 
-        GPIO.output(self.CLK,0)
-        GPIO.output(self.DATA,0)
-        GPIO.output(self.CS_bar,1)
-
+                GPIO.output(self.CLK,0)
+                GPIO.output(self.DATA,0)
+                GPIO.output(self.CS_bar,1)
+        else:
+            if self.CLK < 100:
+                self.CLK=1000000
+            self._dev = SpiDev(0,self.CS_bar)
+            self._dev.mode =0
+            self._dev.max_speed_hz=self.CLK
+            self._dev.bits_per_word = 8
         self.Init(mode)
 
     def Init(self,mode):
@@ -58,9 +74,12 @@ class MAX7219:
     def __del__(self):          # This is automatically called when the class is deleted.
         '''Delete and cleanup.'''
         self.WriteLocChar(0x0C,0x0) # Turn off
-        GPIO.cleanup(self.CLK)
-        GPIO.cleanup(self.DATA)
-        GPIO.cleanup(self.CS_bar)
+        if self.DATA>0:
+            GPIO.cleanup(self.CLK)
+            GPIO.cleanup(self.DATA)
+            GPIO.cleanup(self.CS_bar)
+        else:
+            self._dev.close()
 
     def Clear(self):
         '''Clear the display to all blanks. (it looks off) '''
@@ -76,36 +95,43 @@ class MAX7219:
         self.WriteLocChar(0x0A,B) # Set brightness
 
     def WriteData(self,data):
-        '''Write the 16 bit data to the output using "bit-banged" SPI on the GPIO output line.
+        '''Write the 16 bit data to the output using SPI or
+         "bit-banged" SPI on the GPIO output line.
         This is a "raw" mode write, used internally in these methods.'''
-        GPIO.output(self.CS_bar,0)
+        if self.DATA>0:
+            GPIO.output(self.CS_bar,0)
 
-        for i in range(16):  # send out 16 bits.
-            GPIO.output(self.CLK,0)
-            #time.sleep(0.00001)
-            bit = data & 0x8000
-            GPIO.output(self.DATA,bit)
-            #time.sleep(0.00001)
-            GPIO.output(self.CLK,1)
-            #time.sleep(0.00001)
-            data <<=1
-            if(i==7):
+            for i in range(16):  # send out 16 bits of data sequentially.
                 GPIO.output(self.CLK,0)
-                GPIO.output(self.DATA,0)
-            #    time.sleep(0.00003)
+                #time.sleep(0.00001)
+                bit = data & 0x8000
+                GPIO.output(self.DATA,bit)
+                #time.sleep(0.00001)
+                GPIO.output(self.CLK,1)
+                #time.sleep(0.00001)
+                data <<=1
+                if(i==7):
+                    GPIO.output(self.CLK,0)
+                    GPIO.output(self.DATA,0)
+                #    time.sleep(0.00003)
 
-        GPIO.output(self.DATA,0)
-        GPIO.output(self.CLK,0)
-        GPIO.output(self.CS_bar,1)
+            GPIO.output(self.DATA,0)
+            GPIO.output(self.CLK,0)
+            GPIO.output(self.CS_bar,1)
+        else:
+            self._dev.writebytes([(data>>8)&0xFF,data&0xFF]) # Write the first and second byte from data.
 
     def WriteLocChar(self,loc,dat):
         '''Write dat to loc. If the mode is 1 then dat is a number and loc is the location.
         If mode is 2 then dat is an 8 bit LED position.
         This is used internally to display the numbers/characters.'''
-        out = (loc <<8)
-        out += dat
-        #out += 0b0000000000000000  # Dummy bits
-        self.WriteData(out)
+        if self.DATA>0:
+            out = (loc <<8)
+            out += dat
+            #out += 0b0000000000000000  # Dummy bits
+            self.WriteData(out)
+        else:
+            self._dev.writebytes([loc,dat])
 
     def WriteInt(self,n):
         ''' Write the integer n on the display, shifted left. If n is larger (smaller) than
@@ -187,7 +213,8 @@ class MAX7219:
 
     def __str__(self):
         '''Write something comforting to the user :-) '''
-        print("MAX7219 driver interface. CS_bar={} CLK={} DATA={}",self.CS_bar_pin,self.CLK_pin,self.DATA_pin)
+        if self.DATA>0:
+            print("MAX7219 driver interface. GPIO mode: DATA={} CS_bar={} CLK={}",self.DATA,self.CS_bar,self.CLK)
 
 #
 # The code below turns this module into a program as well
