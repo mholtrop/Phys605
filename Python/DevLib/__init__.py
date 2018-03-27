a40e7daba5f904881b3c17f140fb79315a02e664
Maurik Holtrop <maurik@physics.unh.edu>
2018-03-27 15:26:14 -0400

    Removed the SPI version of MCP320x

--- Python/DevLib/__init__.py
+++ Python/DevLib/__init__.py
@@ -2,10 +2,9 @@
 # Here we initialize the modules.
 #
 
-__all__ = ["MAX7219","MCP320x","MCP320xspi","ISL29125","SN74HC165"]
+__all__ = ["MAX7219","MCP320x","ISL29125","SN74HC165"]
 
 from .MAX7219    import MAX7219
 from .MCP320x    import MCP320x
-from .MCP320xspi import MCP320xspi
 from .ISL29125   import ISL29125
 from .SN74HC165  import SN74HC165
