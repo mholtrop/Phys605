c3ed6d8e6156f7d84f4f79e58741c7e4464700bb
Maurik Holtrop <maurik@physics.unh.edu>
2018-04-09 19:39:12 -0400

    Rewrite the bitbang code to work propoerly with MCP4161 chip.

--- Python/DevLib/MCP4251.py
+++ Python/DevLib/MCP4251.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 #
-# MCP320x
+# Despite the name, this driver should work with:
+# MPC424x,MCP425x,MCP426x,MCP414x,MCP415x,MCP416x
+# where x is 1 or 2
+#
+# Tested with MCP4251 and MPC4161
 #
 # Author: Maurik Holtrop
 #
-# This  module interfaces with an MCP4251 digital programmable resistorself.
+# This  module interfaces with an MCP4251 or MCP4161 digital programmable resistor.
 # See the datasheet for details.
-
+#
+# For the 4161, since the SDI/SDO are a single pin, the big bang interfaces
+# uses a one data wire protocol, so no resistors are needed. On the SpiDev you will
+# need a 1kOhm to 10kOhm resistor from the Raspberry Pi MOSI to MISO pin. Note that
+# this resistor can interfere with other devices on the bus, so bit bang may be preferred.
+#
 # SPI Modes:
 # MODE 0,0
 # In Mode 0,0: SCK idle state = low (VIL), data is clocked in on the SDI pin on
@@ -17,7 +26,17 @@
 #
 # This driver only supports the 0,0 mode.
 #
-
+# If the device is connected to the hardware SPI:
+# The MCP42xx devices have separate SDO and SDI pins, and can communicate at the full 10MHz SPI clockself.
+# For the MCP41x1 devices, you have a SDI/SDO combined pin. This pin is connected directly
+# to the MISO pin, and with a resistor to the MOSI pin on the RaspberryPi.
+# With a 10kOhm resistor I could get reliable data transfer at 250kHz clock speed, but not at 500kHz.
+# With a 1kOhm resistor, I could write at 5 MHz, but not at 10 MHz. Specifications state that
+# reading should not be more than 250kHz in these cases, but I found 5 MHz worked.
+#
+# To accomodate high speed writing for the MCP41x1 devices, the clock speed is adjusted for These
+# chips when using hardware SPI
+#
 
 
 import RPi.GPIO as GPIO
@@ -25,8 +44,13 @@ import spidev
 
 class MCP4251:
 
-    def __init__(self,CS_bar_pin,CLK_pin=1000000,MOSI_pin=0,MISO_pin=0):
-        '''Initialize the code and set the GPIO pins.'''
+    def __init__(self,CS_bar_pin,CLK_pin=10000000,MOSI_pin=0,MISO_pin=0,chip='MCP4251'):
+        '''Initialize the code and set the GPIO pins.
+        If MOSI_pin = 0, the code assumes hardware SPI mode, in which case the
+        CLK_pin number is taken as the maximum clock speed desired. This clock speed
+        is adjusted for the MCP41x1 devices which have a combined SDI/SDO pin.
+        If MOSI_pin !=0, then bit bang mode is used and all the GPIO pin numbers
+        must be specified. For MCP41x1 devices, specify MISO_pin=MOSI_pin.'''
 
         self.CLK = CLK_pin
         self.MOSI = MOSI_pin
@@ -34,7 +58,20 @@ class MCP4251:
         self.CS_bar = CS_bar_pin
 
         self._CMDERR_bit=0b00000010  # Seventh bit send out is the ERROR bit.
+        # Dictionary: chip_name: (channels, readspeed, writespeed)
+        chip_dict={
+            'MCP4251':(2,10000000,10000000),
+            'MCP4161':(1,250000,5000000)}
 
+        if chip in chip_dict:
+            self.Nchannels    =    chip_dict[chip][0]
+            self.MaxReadSpeed =min(chip_dict[chip][1],CLK_pin)
+            self.MaxWriteSpeed=min(chip_dict[chip][2],CLK_pin)
+        else:
+            print("Chip is not known yet, please add to dictionary:",chip)
+            self.Nchannels=2
+            self.MaxReadSpeed=min(250000,CLK_pin)
+            self.MaxWriteSpeed=min(250000,CLK_pin)
 
         if self.MOSI > 0:  # Bing Bang mode
             assert self.MISO !=0 and self.CLK < 32
@@ -42,21 +79,24 @@ class MCP4251:
                 GPIO.setmode(GPIO.BCM)        # Use the BCM numbering scheme
 
             GPIO.setup(self.CLK,GPIO.OUT)     # Setup the ports for in and output
-            GPIO.setup(self.MOSI,GPIO.OUT)
-            GPIO.setup(self.MISO,GPIO.IN)
-            GPIO.setup(self.CS_bar,GPIO.OUT)
+            if self.Nchannels == 1:
+                GPIO.setup(self.MOSI,GPIO.IN)
+                assert self.MISO == self.MOSI
+                self.Direction = 0
+            else:
+                GPIO.setup(self.MOSI,GPIO.OUT)
+                GPIO.setup(self.MISO,GPIO.IN)
+                GPIO.output(self.MOSI,0)          # Set the Master Out low
 
+            GPIO.setup(self.CS_bar,GPIO.OUT)
             GPIO.output(self.CLK,0)           # Set the clock low.
-            GPIO.output(self.MOSI,0)          # Set the Master Out low
             GPIO.output(self.CS_bar,1)        # Set the CS_bar high
 
         else:
-            self._dev = spidev.SpiDev(0,self.CS_bar) # Start a SpiDev device
-            self._dev.mode = 0                    # Set SPI mode (phase)
-            self._dev.max_speed_hz = self.CLK     # Set the data rate
-            self._dev.bits_per_word = 8           # Number of bit per word. ALWAYS 8
-
-        self.Reset()
+            self._dev = spidev.SpiDev(0,self.CS_bar)     # Start a SpiDev device
+            self._dev.mode = 0                           # Set SPI mode (phase)
+            self._dev.max_speed_hz = self.MaxWriteSpeed  # Set the data rate
+            self._dev.bits_per_word = 8                  # Number of bit per word. ALWAYS 8
 
     def __del__(self):
         ''' Cleanup the GPIO before being destroyed '''
@@ -64,91 +104,168 @@ class MCP4251:
             GPIO.cleanup(self.CS_bar)
             GPIO.cleanup(self.CLK)
             GPIO.cleanup(self.MOSI)
-            GPIO.cleanup(self.MISO)
+            if self.Nchannels > 1:
+                GPIO.cleanup(self.MISO)
 
-    def SendBit(self,bit):
-        ''' Send out a single bit, and pulse clock.'''
+    def SendBits(self,bits,num):
+        ''' Send out a stream of bits, MSB first, on GPIO'''
         if self.MOSI == 0:
             return
         #
         # The input is read on the rising edge of the clock.
         #
-        GPIO.output(self.CLK,0)    # Return clock to zero.
-        GPIO.output(self.MOSI,bit) # Set the bit.
-        rbit = int(GPIO.input(self.MISO)) # Read the return bit.
-        GPIO.output(self.CLK,1)    # Rising edge sends data
-        return(rbit)
+        if self.Nchannels == 1 and self.Direction == 0:
+            GPIO.setup(self.MOSI,GPIO.OUT)
+            self.Direction = 1
 
-    def ReadBit(self):
+        for i in range(num-1,-1,-1):
+            bit = (bits>>i)&0b1
+            GPIO.output(self.CLK,0)    # Return clock to zero.
+            GPIO.output(self.MOSI,bit) # Set the bit.
+            GPIO.output(self.CLK,1)    # Rising edge sends data
+        GPIO.output(self.CLK,0)    # Return clock to zero.
+
+
+
+    def ReadBits(self,num):
         ''' Read a single bit from the ADC and pulse clock.'''
         if self.MOSI == 0:
             return 0
-        #
-        # The output is going out on the falling edge of the clock,
-        # and is to be read on the rising edge of the clock.
 
-        # Clock should be already low, and data should already be set.
-        GPIO.output(self.CLK,1) # Set the clock high. Ready to read.
-        bit = GPIO.input(self.MISO) # Read the bit.
-        GPIO.output(self.CLK,0) # Return clock low, next bit will be set.
+        if self.Nchannels == 1 and self.Direction == 1:
+            GPIO.setup(self.MOSI,GPIO.IN)
+            self.Direction = 0
 
-        return(bit)
+        out=0
+        for i in range(num):
+            out <<= 1               # Shift all the bits left.
+            GPIO.output(self.CLK,1) # Set the clock high. Ready to read.
+            bit = GPIO.input(self.MISO) # Read the next bit.
+            GPIO.output(self.CLK,0) # Return clock low, next bit will be set.
+            out+=bit                # Add bit to the output.
+
+        return(out)
 
     def WriteWords(self,data):
         '''Write the data in "data" to the device, and return the answer.
-        Data is a list of 8-bit words to be written. The same number of words is returned.'''
+        Data is a list of 8-bit words to be written. The same number of words is returned.
+        Note that bit-banged single pin operation is treaded seprately.'''
 
         if self.MOSI == 0:
             answer=self._dev.xfer2(data)
-        else:
+        else:                          #### BIT BANG CODE
             GPIO.output(self.CLK,0)      # Make sure clock starts low.
+            if self.Nchannels == 1:
+                GPIO.setup(self.MOSI,GPIO.OUT) # Turn on the output.
             GPIO.output(self.MOSI,0)
             GPIO.output(self.CS_bar,0)   # Start sequence
             ans=0
             answer=[]
-            for d in data:
-                for i in range(8):       # We send out 8 bit words
-                    bit= d & 0x80     # The highest bit is send first.
-                    rbit=self.SendBit(bit>0) # Write out the high bit
-                    d= (d<<1)&0xFF # Shift all the bits one position left and truncate.
-                    ans = ans<<1
-                    ans+= rbit
-                answer.append(ans)
-            GPIO.output(self.CLK,0)     # End, make sure clock is low.
-            GPIO.output(self.MOSI,0)    # and MOSI is low.
+            #
+            # Every sequence starts with a 4 bit ADDRESS
+            # followed by a 2 bit "command":
+            #   00  = write command
+            #   11  = read command
+            #   01  = Increment
+            #   10  = Decrement
+            #
+            # Next is acknowledge bit, which is a read.
+            # This is then follow by bit8 of the write, or read, or a dummy.
+            # For write or read the next 8 bits are data.
+            #
+            l=0
+            while l < len(data):
+                d = data[l]
+                address = (d>>4)
+                command = ((d & 0b1100)>>2)
+                highbit = d&0b01
+                self.SendBits(address,4) # Send the 4 address bits.
+                self.SendBits(command,2) # Send the 2 command bits.
+                ack_bit=self.ReadBits(1) # Read the acknowledge bit.
+                if command == 0b00:      # Write operation
+                    self.SendBits(highbit,1)
+                    self.SendBits(data[l+1],8)
+                    l += 1
+                    answer.append( (ack_bit<<1))
+                    answer.append(0)
+                elif command == 0b11:      # Read Operation.
+                    ans = self.ReadBits(1) # Read the high bit.
+                    ans += (ack_bit<<1)
+                    answer.append(ans)
+                    ans = self.ReadBits(8) # Read the answer
+                    answer.append(ans)
+                    l += 1                 # Skip the dummy word.
+                else:
+                    ans = self.ReadBits(1) # Read the high bit.
+                    ans += (ack_bit<<1)
+                    answer.append(ans)     # There is no dummy word!
+
+                l += 1
+
             GPIO.output(self.CS_bar,1)  # Deselect the chip.
+            GPIO.output(self.CLK,0)     # End, make sure clock is low.
+            if self.Nchannels==1:
+                if self.Direction == 0:
+                    GPIO.setup(self.MOSI,GPIO.OUT) # Turn on the output.
+                    self.Direction == 1
+
+            GPIO.output(self.MOSI,0)    # and MOSI is low.
         return(answer)  # Return answer
+
+    def ReadAddress(self,addr):
+        '''Read an address from the chip and return it.
+        Returns None if the read had an error.'''
+        low_word=0xFF                               # The read part must be high for the 4161 chips.
+        command = ((addr&0x0F)<<4) + 0b1111     # Address (4bits), read command, 11
+        if self.MOSI==0:                        # Set slower read on spidev.
+            self._dev.max_speed_hz=self.MaxReadSpeed
+
+        ans=self.WriteWords([command,low_word])
+
+        if self.MOSI==0:                        # Restore to write speed on spidev.
+            self._dev.max_speed_hz=self.MaxWriteSpeed
+
+        if( (ans[0]&self._CMDERR_bit)>0):
+            return(ans[1]+ ( (ans[0]&0b01)<<8) )
+        else:
+            return(None)                    # Error
+
+    def WriteAddress(self,addr,data):
+        '''Write the wiper value from the chip and return it.
+        Returns None if the read had an error.'''
+        low_word= data&0xFF                  # Low 8 bits of data
+        command = ((addr&0x0F)<<4) + 0b0010 + ((data&0x100)>>8)   # Address (4bits), write (00), 1, highbit
+        ans=self.WriteWords([command,low_word])
+        if( (ans[0]&self._CMDERR_bit)>0):
+            return(ans[1]+ ( (ans[0]&0b01)<<8) )
+        else:
+            return(None)                    # Error
 
 
     def SetWiper(self,setpoint=128,wiper=0):
         '''Set the wiper to setpoint. Setpoint can be between 0 and MAX inclusive,
         where MAX is 256 for the MCP4251
         Returns True if status is OK'''
-        set_low = (setpoint & 0xFF)             # Lower 8 bits
-        command = ((wiper&0b01)<<4) + ((setpoint>>8) & 0b01) # Address (0 or 1) and command (00) and hi bit
-        ans=self.WriteWords([command,set_low]) # Send/receive data.
-        return( (ans[0]&self._CMDERR_bit)>0 )    # Return True if status OK
+        addr = wiper&0b01 # Address (0 or 1) and command (00) and hi bit
+        ans=self.WriteAddress(addr,setpoint) # Send/receive data.
+        return(ans)    # Return True if status OK
 
     def ReadWiper(self,wiper=0):
         '''Read the wiper value from the chip and return it.
         Returns None if the read had an error.'''
-        set_low=0
-        command = ((wiper&0b01)<<4) + 0b00001100   # Address (0 or 1) and read command.
-        ans=self.WriteWords([command,set_low])
-        if( (ans[0]&self._CMDERR_bit)>0):
-            return(ans[1]+ ( (ans[0]&0b01)<<8) )
-        else:
-            return(None)                    # Error
+        addr = wiper&0b01
+        ans=self.ReadAddress(addr)
+        return(ans)
 
     def IncrementWiper(self,wiper=0):
         '''Increment the wiper by one setting. Returns True is successful'''
-        command = ((wiper&0b01)<<4) + 0b0100   # Address (0 or 1) and INCR command.
+        command = ((wiper&0b01)<<4) + 0b0111   # Address (0 or 1) and INCR command.
         ans=self.WriteWords([command])
         return((ans[0]&self._CMDERR_bit)>0)
 
     def DecrementWiper(self,wiper=0):
         '''Decrement the wiper by one setting. Returns True is successful'''
-        command = ((wiper&0b01)<<4) + 0b1000   # Address (0 or 1) and DECR command.
+        command = ((wiper&0b01)<<4) + 0b1011   # Address (0 or 1) and DECR command.
         ans=self.WriteWords([command])
         return((ans[0]&self._CMDERR_bit)>0)
 
@@ -156,7 +273,8 @@ class MCP4251:
         '''Reset the chip:
         Connect the wipers and A,B contacts and set them in the middle.'''
         self.SetWiper(128,0)
-        self.SetWiper(128,1)
+        if self.Nchannels == 2:
+            self.SetWiper(128,1)
         self.WriteWords([0b01000001,0xFF])        # Write the TCON to all connected, no shutdown.
 
     def DisconnectWiper(self,wiper=0):
