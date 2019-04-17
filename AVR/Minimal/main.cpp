d77e67b3d25ae879e395b658af114cc5fee62943
Maurik Holtrop <maurik@physics.unh.edu>
2019-04-17 17:03:12 -0400

    Simpler version

--- AVR/Minimal/main.cpp
+++ AVR/Minimal/main.cpp
@@ -1,30 +1,40 @@
 /*
  *  Minimal.cpp
  *
- *  Minimal program for directly talking to an Atmega168
+ *  Minimal program for directly talking to an Atmega328p
  *
- *  Created on: April 2, 2015
+ *  This code shows the "advanced" way of programming an AVR microprocessor.
+ *  We use the information from the datasheet and from the AVR Software Developement Kit (SDK)
+ *
+ *  To keep the ease of some function from the Arduino world, such as the Serial class, we import
+ *  the Arduino header and link against the Arduino libraries.
+ *
+ *  Created on: April 2, 2015, updated April 17, 2019
  *      Author: Maurik
  */
 
 #include <avr/io.h>
 #include "Arduino.h"
 #include "HardwareSerial.h"
+#include "Sensirion.h"
 
 int
 main (void) {
 
-  sei();  // Enable interrupts:  __asm__ __volatile__ ("sei" ::: "memory");
+  init(); // The Arduino init() function is needed for timer and delay(ms) to function properly.
+
   Serial.begin(115200);
-  Serial.print("Hello this is the atmega168 starting up. V-0.0.2\n");
+  Serial.print("Hello this is the atmega328 starting up. V-0.0.2\n\r");
 
   // Set interrupts enabled.
-  DDRB = 0x02;                   // Scope to PB1
+  DDRB = 0x02;    // Pin PB1 = chip pin 15 is output. Put Scope here.
   PORTB = 0x02;   // ON
-  DDRD = 0x80; // 0b10000000; // PD7 is output.
-  PORTD = 0x80; // ON
+  PORTB = 0x00;   // OFF
+  DDRD = 0x80;    // Pin PD7 = chip pin 13 is output. Put LED (w. resistor!) here.
+  PORTD = 0x00;   // OFF
 
   Serial.print("Loop top..\n");
+
   int loopcount=0;
   while (1) {
       if( (loopcount++)%100 == 0){
@@ -33,44 +43,61 @@ main (void) {
       }
       Serial.print(".");
       for (unsigned char i = 0; i < 255; i++) {
-        for (unsigned char i = 0; i < 255; i++) {
-//        PORTB &= 0xFD; // PB1 OFF
-//        PORTB |= 0x02; // PB1 ON
-//        PORTB &= 0xFD; // PB1 OFF
-//        PORTB |= 0x02; // PB1 ON
-//        PORTB &= 0xFD; // PB1 OFF
-//        PORTB |= 0x02; // PB1 ON
+        for (unsigned char i = 0; i < 255; i++) {  // Turn on and off PB1 port quickly 8 times for every change on PD7
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
+	  PORTB |= 0x02; // PB1 ON
+	  PORTB &= 0xFD; // PB1 OFF
 
-
-          // Set bit 1 on PORTB 0x05 The _SFR_IO_ADDR subtracts 0x20 for port address
-        asm volatile(
-            "sbi %0, %1 " "\n\t"
-            "cbi %0, %1 " "\n\t"
-            "sbi %0, %1 " "\n\t"
-            "cbi %0, %1 " "\n\t"
-            "sbi %0, %1 " "\n\t"
-            "cbi %0, %1 " "\n\t"
-            "nop " "\n\t"
-            "nop " "\n\t"
-            "nop " "\n\t"
-            "nop " "\n\t"
-            "nop " "\n\t"
-            "nop " "\n\t"
-            "nop " "\n\t"
-            "nop " "\n\t"
-            "sbi %2, %1 " "\n\t"  // Toggle the pin on port.
-            "sbi %2, %1 " "\n\t"
-            "sbi %2, %1 " "\n\t"
-            "sbi %2, %1 " "\n\t"
-            "sbi %2, %1 " "\n\t"
-            "sbi %2, %1 " "\n\t"
-            "sbi %2, %1 " "\n\t"
-            "sbi %2, %1 " "\n\t"
-            ::"I" (_SFR_IO_ADDR(PORTB)),"I" (PB1),"I" (_SFR_IO_ADDR(PINB))
-        );
+	  // Here is an even more advaced method of changing the output pins. 
+	  // If you want absolete perfect control of the chip at maximum speed, you
+	  // need to use Assembly language.
+	  // Typically, we avoid doing this, and have the C compiler do the work.
+	  //
+	  //
+        // Set bit 1 on PORTB 0x05 The _SFR_IO_ADDR subtracts 0x20 for port address
+        // asm volatile(
+        //     "sbi %0, %1 " "\n\t"
+        //     "cbi %0, %1 " "\n\t"
+        //     "sbi %0, %1 " "\n\t"
+        //     "cbi %0, %1 " "\n\t"
+        //     "sbi %0, %1 " "\n\t"
+        //     "cbi %0, %1 " "\n\t"
+        //     "nop " "\n\t"
+        //     "nop " "\n\t"
+        //     "nop " "\n\t"
+        //     "nop " "\n\t"
+        //     "nop " "\n\t"
+        //     "nop " "\n\t"
+        //     "nop " "\n\t"
+        //     "nop " "\n\t"
+        //     "sbi %2, %1 " "\n\t"  // Toggle the pin on port.
+        //     "sbi %2, %1 " "\n\t"
+        //     "sbi %2, %1 " "\n\t"
+        //     "sbi %2, %1 " "\n\t"
+        //     "sbi %2, %1 " "\n\t"
+        //     "sbi %2, %1 " "\n\t"
+        //     "sbi %2, %1 " "\n\t"
+        //     "sbi %2, %1 " "\n\t"
+        //     ::"I" (_SFR_IO_ADDR(PORTB)),"I" (PB1),"I" (_SFR_IO_ADDR(PINB))
+        // );
       }
-    }
     PORTD ^= 0x80;  // XOR = toggle bit 7.
+    }
+
   }
 }
 
