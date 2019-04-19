3f4c137545e8ff6b397fc1787e9c187c9f91f093
Maurik Holtrop <maurik@physics.unh.edu>
2019-04-19 12:40:29 -0400

    fix error

--- Python/DevLib/__init__.py
+++ Python/DevLib/__init__.py
@@ -2,12 +2,13 @@
 # Here we initialize the modules.
 #
 
-__all__ = ["MAX7219","MCP320x","ISL29125","SN74HC165","MCP4251","AD9850","BME280"]
+__all__ = ["MAX7219","MCP320x","ISL29125","SN74HC165","MCP4251","MCP4725","AD9850","BME280"]
 
 from .MAX7219    import MAX7219
 from .MCP320x    import MCP320x
 from .ISL29125   import ISL29125
 from .SN74HC165  import SN74HC165
 from .MCP4251    import MCP4251
+from .MCP4725    import MCP4725
 from .AD9850     import AD9850
 from .BME280     import BME280
