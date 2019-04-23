beaab74a8ad843934f153089c8d9276701e017bb
Maurik Holtrop <maurik@physics.unh.edu>
2019-04-22 20:17:52 -0400

    Add APA102

--- Python/DevLib/__init__.py
+++ Python/DevLib/__init__.py
@@ -6,6 +6,7 @@ __all__ = ["AD9850","BME280","DS3231","ISL29125","MAX7219","MCP320x","MCP4251","
 
 from .AD9850     import AD9850
 from .ADS1115    import ADS1115
+from .APA102     import APA102
 from .BME280     import BME280
 from .DS3231     import DS3231
 from .ISL29125   import ISL29125
