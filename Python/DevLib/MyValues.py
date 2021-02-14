a4f4e6a7410dd64047f33c703cc0f6ead3c5ae8d
Maurik Holtrop <maurik@physics.unh.edu>
2021-02-14 14:41:49 -0500

    Update the MCP320x, more modern approach, cleanup

---
+++ Python/DevLib/MyValues.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# MyValues
+#
+# Author: Maurik Holtrop
+#
+# This is a helper class to allow values to be set like a list for any of the chips.
+#
+
+class MyValues:
+    """Class for getting the value of the chip, which mimics a list."""
+
+    def __init__(self, getter, max_num):
+        self._getter = getter
+        self._MAX = max_num
+        self._n = 0
+
+    def __getitem__(self, idx):
+        return self._getter(idx)
+
+    def __setitem__(self, idx, val):
+        raise ValueError("The ADC values cannot be written to, only read.")
+
+    def __len__(self):
+        return self._MAX
+
+    def __iter__(self):
+        self._n = 0
+        return self
+
+    def __next__(self):
+        if self._n < len(self):
+            result = self[self._n]
+            self._n += 1
+            return result
+        else:
+            raise StopIteration
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        tmplist = [x for x in self]
+        return str(tmplist)
+
